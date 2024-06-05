801274a93b42e8ab1b27106e11f8a2dc5544e25b
hahosameer <ks4063180@gmail.com>
2024-06-05 21:42:01 +0500

    token bug fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,48 +26,44 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
     const { password, ...other } = user._doc;
 
-    res.cookie("access_token", token, {
+    res
+      .cookie("access_token", token, {
         httpOnly: true,
-      }).status(200).json(other);
-
-
+      })
+      .status(200)
+      .json({ ...other, token });
   } catch (err) {
     next(err);
   }
 };
 
+export const googleAuth = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
 
-export const googleAuth = async (req, res , next) => {
-try {
-  const user = await  User.findOne({email: req.body.email})
-  if(user){
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
-   res
-     .cookie("access_token", token, {
-       httpOnly: true,
-     })
-     .status(200)
-     .json(user._doc);
-
-  }else{
-    const newUser = new User({
-      ...req.body,
-      fromGoogle: true
-    })
-    const saveUser = await newUser.save();
-        const token = jwt.sign(
-          { id: saveUser._id },
-          process.env.JWT_SECRET_KEY
-        );
-          res
-            .cookie("access_token", token, {
-              httpOnly: true,
-            })
-            .status(200)
-            .json(saveUser._doc);
-
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json({ ...user._doc, token });
+    } else {
+      const newUser = new User({
+        ...req.body,
+        fromGoogle: true,
+      });
+      const saveUser = await newUser.save();
+      const token = jwt.sign({ id: saveUser._id }, process.env.JWT_SECRET_KEY);
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(saveUser._doc);
+    }
+  } catch (error) {
+    next(error);
   }
-} catch (error) {
-  next(error)
-}
-}
+};
--- virifyToken.js
+++ virifyToken.js
@@ -1,8 +1,11 @@
 import jwt from "jsonwebtoken";
 import {createError} from './error.js'
 export const virifyToken = async (req, res, next) => {
+    console.log("req",req.cookies) 
+    const token = req.headers.token;
 
-    const token = await req.cookies.access_token 
+    
+    // const token = await req.cookies.access_token 
     if(!token) return next(createError(401 , "You are not authenticated"))
 
     jwt.verify(token, process.env.JWT_SECRET_KEY , (err , user) => {
